af860be23b354808040f1f8cac8242c8c8aae443
shrieffmohamedd12-code <shrieffmohamedd12@gmail.com>
2026-08-04 11:48:13 +0300

    Update db.js

--- db.js
+++ db.js
@@ -403,3 +403,27 @@ export async function receiveOrder(order, internalPo, sizeBreakdown, totalQty, n
     if (txErr) throw txErr;
   }
 }
+const db = {
+  fetchAll,
+  fetchProfiles,
+  signInByName,
+  signOut,
+  getCurrentProfile,
+  signUpFirstAdmin,
+  createUser,
+  deleteUser,
+  addFactory,
+  removeFactory,
+  addItem,
+  addColorToItem,
+  setItemMinQty,
+  applyMovement,
+  importItemsFromExcel,
+  replaceWarehouseFromExcel,
+  addOrder,
+  importOrders,
+  setOrderStatus,
+  receiveOrder,
+};
+
+export default db;
